f8033ac27b4c8b9365ce825e1b95a7b37dcc0bbb
Vimal Kumar <vimal78@gmail.com>
2026-03-28 14:25:21 +0530

    test: add list-of-maps linearization test

--- tests/test_linearizer.py
+++ tests/test_linearizer.py
@@ -72,3 +72,70 @@ def test_nested_mapping():
     assert nodes[4].depth == 1
     assert nodes[4].sibling_index == 1
     assert nodes[4].parent_path == "metadata.namespace"
+
+
+def test_list_of_maps():
+    yaml_str = (
+        "containers:\n"
+        "- name: webserver1\n"
+        "  image: nginx:1.6\n"
+        "- name: database-server\n"
+        "  image: mysql-3.2\n"
+    )
+    linearizer = YamlLinearizer()
+    nodes = linearizer.linearize(yaml_str)
+
+    assert len(nodes) == 9
+
+    assert nodes[0].token == "containers"
+    assert nodes[0].node_type == NodeType.KEY
+    assert nodes[0].depth == 0
+    assert nodes[0].parent_path == ""
+
+    assert nodes[1].token == "name"
+    assert nodes[1].node_type == NodeType.LIST_KEY
+    assert nodes[1].depth == 1
+    assert nodes[1].sibling_index == 0
+    assert nodes[1].parent_path == "containers.0"
+
+    assert nodes[2].token == "webserver1"
+    assert nodes[2].node_type == NodeType.LIST_VALUE
+    assert nodes[2].depth == 1
+    assert nodes[2].sibling_index == 0
+    assert nodes[2].parent_path == "containers.0.name"
+
+    assert nodes[3].token == "image"
+    assert nodes[3].node_type == NodeType.LIST_KEY
+    assert nodes[3].depth == 1
+    assert nodes[3].sibling_index == 1
+    assert nodes[3].parent_path == "containers.0"
+
+    assert nodes[4].token == "nginx:1.6"
+    assert nodes[4].node_type == NodeType.LIST_VALUE
+    assert nodes[4].depth == 1
+    assert nodes[4].sibling_index == 1
+    assert nodes[4].parent_path == "containers.0.image"
+
+    assert nodes[5].token == "name"
+    assert nodes[5].node_type == NodeType.LIST_KEY
+    assert nodes[5].depth == 1
+    assert nodes[5].sibling_index == 0
+    assert nodes[5].parent_path == "containers.1"
+
+    assert nodes[6].token == "database-server"
+    assert nodes[6].node_type == NodeType.LIST_VALUE
+    assert nodes[6].depth == 1
+    assert nodes[6].sibling_index == 0
+    assert nodes[6].parent_path == "containers.1.name"
+
+    assert nodes[7].token == "image"
+    assert nodes[7].node_type == NodeType.LIST_KEY
+    assert nodes[7].depth == 1
+    assert nodes[7].sibling_index == 1
+    assert nodes[7].parent_path == "containers.1"
+
+    assert nodes[8].token == "mysql-3.2"
+    assert nodes[8].node_type == NodeType.LIST_VALUE
+    assert nodes[8].depth == 1
+    assert nodes[8].sibling_index == 1
+    assert nodes[8].parent_path == "containers.1.image"
