259187614b048bb41c418fa566f7073f126ce099
Vimal Kumar <vimal78@gmail.com>
2026-03-28 19:10:01 +0530

    feat: update package exports with Phase 2 modules

--- yaml_bert/__init__.py
+++ yaml_bert/__init__.py
@@ -1,15 +1,26 @@
 """YAML-BERT: Attention on Kubernetes Structured Data."""
 
+from yaml_bert.config import YamlBertConfig
 from yaml_bert.types import NodeType, YamlNode
 from yaml_bert.linearizer import YamlLinearizer
 from yaml_bert.vocab import Vocabulary, VocabBuilder
 from yaml_bert.annotator import DomainAnnotator
+from yaml_bert.embedding import YamlBertEmbedding
+from yaml_bert.model import YamlBertModel
+from yaml_bert.dataset import YamlDataset, collate_fn
+from yaml_bert.trainer import YamlBertTrainer
 
 __all__ = [
+    "YamlBertConfig",
     "NodeType",
     "YamlNode",
     "YamlLinearizer",
     "Vocabulary",
     "VocabBuilder",
     "DomainAnnotator",
+    "YamlBertEmbedding",
+    "YamlBertModel",
+    "YamlDataset",
+    "collate_fn",
+    "YamlBertTrainer",
 ]
